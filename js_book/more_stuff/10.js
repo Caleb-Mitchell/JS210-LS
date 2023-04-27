a420e603ee2a15e6eb5920fbc0113bfb81679bfa
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-04-26 21:13:42 -0500

    Finish the book

---
+++ js_book/more_stuff/10.js
@@ -0,0 +1,6 @@
+/*
+
+The numeric value `5`. JavaScript first coerces `'5'` to a number, increments
+this by 1 returning '51', but ultimately because this is the post-increment form
+it returns the original value, `5`.
+*/

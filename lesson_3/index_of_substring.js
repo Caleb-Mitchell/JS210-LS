00c0989ea6bcf80b8d4c12f60f0bd79ae405a95f
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-19 00:23:54 -0500

    Finish another exercise

--- lesson_3/index_of_substring.js
+++ lesson_3/index_of_substring.js
@@ -1,20 +1,59 @@
+/*
 
+problem:
+The indexOf function searches for the first instance of a substring in
+firstString that matches the string secondString, and returns the index of the
+character where that substring begins.
+
+input: 2 strings
+output: an integer, the index of the character of the first matching substring
+
+rules:
+- may use square brackets
+- may use the length property on strings
+- may not use built in String methods
+
+mm:
+for each char, idx in firstString:
+  return the index if all chars of secondString match from that idx in firstString
+*/
 
 function indexOf(firstString, secondString) {
-  // create all substrings, as nested arrays, where second element is the
-  // index that the substring starts at
-
-  // filter for the first element equal to the second
-
-  // return the index of that subarray
-
-  let subStrings = [];
-
-  let outerChars = [...firstString];
-  for (let outerIdx = 0; outerIdx < outerChars.length; outerIdx++) {
-    console.log(outerChars[outerIdx])
-  };
-
+  for (let outerIdx = 0; outerIdx < firstString.length; outerIdx++) {
+    if (
+      [...secondString].every((innerChar, innerIdx) => {
+        return [...firstString][outerIdx + innerIdx] === innerChar;
+      })
+    ) {
+      return outerIdx;
+    }
+  }
+  return -1;
 }
 
-indexOf('Some strings', 's');                      // 5
+function lastIndexOf(firstString, secondString) {
+  let indices = [];
+  for (let outerIdx = 0; outerIdx < firstString.length; outerIdx++) {
+    if (
+      [...secondString].every((innerChar, innerIdx) => {
+        return [...firstString][outerIdx + innerIdx] === innerChar;
+      })
+    ) {
+      indices.push(outerIdx);
+    }
+  }
+  if (indices.length > 0) {
+    return indices[indices.length - 1];
+  } else {
+    return -1;
+  }
+}
+
+console.log(indexOf("Some strings", "s")); // 5
+console.log(indexOf("Blue Whale", "Whale")); // 5
+console.log(indexOf("Blue Whale", "Blute")); // -1
+console.log(indexOf("Blue Whale", "leB")); // -1
+
+console.log(lastIndexOf("Some strings", "s")); // 11
+console.log(lastIndexOf("Blue Whale, Killer Whale", "Whale")); // 19
+console.log(lastIndexOf("Blue Whale, Killer Whale", "all")); // -1

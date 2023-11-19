3bf1a9a5ca90cbfa32e6576be85ab29d9cf462dd
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-11-19 00:04:40 -0600

    Continue implementing built in js array methods

--- lesson_4/array_ops_slice_splice_concat_join/1.js
+++ lesson_4/array_ops_slice_splice_concat_join/1.js
@@ -1,3 +1,15 @@
-function slice(array, startIdx, endIdx) {
-
+function push(array, otherValue) {
+  array[array.length] = otherValue;
+  return array.length;
 }
+
+function slice(array, startIdx, endIdx) {
+  let newArr = [];
+  for (idx = startIdx; idx < endIdx; idx++) {
+    push(newArr, array[idx]);
+  }
+  return newArr;
+}
+
+console.log(slice([1, 2, 3, 4, 5], 0, 2));                      // [ 1, 2 ]
+console.log(slice(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1, 3));  // [ 'b', 'c' ]
---
+++ lesson_4/array_ops_slice_splice_concat_join/2.js
@@ -0,0 +1,38 @@
+function push(array, otherValue) {
+  array[array.length] = otherValue;
+  return array.length;
+}
+
+function pop(array) {
+    if (array.length === 0) {
+    return undefined;
+  }
+
+  let lastEle = array[array.length - 1];
+  array.length = array.length - 1;
+  return lastEle;
+}
+
+function splice(array, startIdx, numValues) {
+  // need to also remove the elements from array that are added to newArr
+  let newArr = [];
+  for (let idx = 0; idx < numValues; idx++) {
+    push(newArr, array[idx + startIdx]);
+  }
+  // create another updatedArr
+  // iterate over array
+  // push values to original array unless idx is between startIdx and (startIdx + numValues)
+  // reassign array to updatedArr
+  let updatedArr = [];
+  for (let idx = 0; idx < array.length; idx++) {
+    if ((idx < startIdx || idx >= (startIdx + numValues))) {
+      push(updatedArr, array[idx])
+    }
+  }
+  array = updatedArr;
+  return newArr;
+}
+
+let count = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(splice(count, 2, 5));                   // [ 3, 4, 5, 6, 7 ]
+console.log(count);                                 // [ 1, 2, 8 ]
